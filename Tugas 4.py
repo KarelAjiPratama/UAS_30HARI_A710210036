7f4b6591896076389bd4340e9f0a2da8df9515ee
Karel Aji Pratama <147335637+KarelAjiPratama@users.noreply.github.com>
2024-06-08 15:44:05 +0700

    Create Tugas 4.py
    Tugas PBO 4

---
+++ Tugas 4.py
@@ -0,0 +1,8 @@
+bilangan = float(input("Masukkan sebuah bilangan: "))
+
+if bilangan > 0:
+    print("Bilangan positif")
+elif bilangan == 0:
+    print("Bilangan nol")
+else:
+    print("Bilangan negatif")
